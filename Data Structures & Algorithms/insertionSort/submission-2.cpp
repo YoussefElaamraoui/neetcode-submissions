522c83d96a448d4d3d4b837d59d26451c36c347e
Youssef El aamraoui <elaamraoui.youssef@outlook.com>
2026-08-13 10:06:00 +0200

    Add: insertionSort - submission-2

---
+++ Data Structures & Algorithms/insertionSort/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<Pair>> insertionSort(vector<Pair>& pairs) {
+       vector<vector<Pair>>res;
+
+       for (int i =0;i<pairs.size();i++){
+        int j=i-1;
+        while(j>=0 && pairs[j].key>pairs[j+1].key){
+            swap(pairs[j],pairs[j+1]);
+            j--;
+        }
+
+        res.push_back(pairs);
+       }
+
+       return res;
+    }
+};
